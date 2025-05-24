a57478ecb4947d41e1f6fb1e121adcc49dbbe673
lsRTD <212518205@qq.com>
2025-05-24 21:15:18 +0800

    第一次初始化“

---
+++ Source/Kitsune/Public/Characters/KitsuneCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/CharacterBase.h"
+#include "KitsuneCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KITSUNE_API AKitsuneCharacter : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+};
